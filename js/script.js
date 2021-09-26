dc8ffe9bd51888a25aad23df01154cf449015c9b
linox2000 <mllino1@hotmail.com>
2021-09-26 17:20:09 +0100

    finsihed the mobile nav

--- js/script.js
+++ js/script.js
@@ -1,30 +1,78 @@
-console.log("Hello world!");
+// console.log("Hello world!");
 
-const myName = "Jonas Schmedtmann";
-const h1 = document.querySelector(".heading-primary");
-console.log(myName);
-console.log(h1);
+// const myName = "Jonas Schmedtmann";
+// const h1 = document.querySelector(".heading-primary");
+// console.log(myName);
+// console.log(h1);
 
-// h1.addEventListener("click", function () {
-//   h1.textContent = myName;
-//   h1.style.backgroundColor = "red";
-//   h1.style.padding = "5rem";
-// });
+// // h1.addEventListener("click", function () {
+// //   h1.textContent = myName;
+// //   h1.style.backgroundColor = "red";
+// //   h1.style.padding = "5rem";
+// // });
 
-///////////////////////////////////////////////////////////
-// Set current year
-const yearEl = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-yearEl.textContent = currentYear;
+// ///////////////////////////////////////////////////////////
+// // Set current year
+// const yearEl = document.querySelector(".year");
+// const currentYear = new Date().getFullYear();
+// yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
 const btnNavEl = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
 
-btnNacEl.addEventListener('click',function(){
+btnNavEl.addEventListener('click',function(){
   headerEl.classList.toggle('nav-open')
+});
+// Smooth scrooling animation
+
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function(link){
+  link.addEventListener('click',function(e){
+    e.preventDefault();
+    const href = link.getAttribute('href');
+    // console.log(href)
+
+    // Scrool back to top
+    if(href === '#') window.scrollTo({
+      top:0,
+      behavior:'smooth'
+    });
+    // Scrool to other links
+    if(href !== '#' && href.startsWith('#')){
+     const sectionEl= document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior:'smooth'
+      });
+    } 
+    // Close mobile navigation
+    if(link.classList.contains('main-nav-link'))
+    headerEl.classList.toggle('nav-open');
+  });
+});
+
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector('.section-hero');
+const obs = new IntersectionObserver(function(entries){
+  const ent = entries[0];
+  console.log(ent);
+  if(!ent.isIntersecting)
+  document.body.classList.add('sticky')
+  
+  if(ent.isIntersecting)
+  document.body.classList.remove('sticky')
+},{
+  // In the Viewport
+  root:null,
+  threshold:0,
+  rootMargin: '-80px',
 })
+obs.observe(sectionHeroEl);
+
+
+
 
 
 ///////////////////////////////////////////////////////////
